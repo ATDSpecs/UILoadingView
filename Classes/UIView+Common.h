f1bd7be90abf3e3d665c995c33b70b787e289cfe
libo <libo@atidesoft.com>
2017-12-19 16:14:48 +0800

    initial UILoadingView

---
+++ Classes/UIView+Common.h
@@ -0,0 +1,32 @@
+//
+//  UIView+Common.h
+//  Coding_iOS
+//
+//  Created by 王 原闯 on 14-8-6.
+//  Copyright (c) 2014年 Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import<QuartzCore/QuartzCore.h>
+
+@class EaseLoadingView;
+
+
+@interface UIView (Common)
+
+
+#pragma mark LoadingView
+@property (strong, nonatomic) EaseLoadingView *loadingView;
+- (void)beginLoading;
+- (void)endLoading;
+
+@end
+
+@interface EaseLoadingView : UIView
+@property (strong, nonatomic) UIImageView *loopView, *monkeyView;
+@property (assign, nonatomic, readonly) BOOL isLoading;
+- (void)startAnimating;
+- (void)stopAnimating;
+@end
+
+
